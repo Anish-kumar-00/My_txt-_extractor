fb50c35a14a648abe543e528099e6685de4db0cd
Anish Kumar <ay6704197@gmail.com>
2026-09-03 19:55:23 +0530

    update config.py

--- config.py
+++ config.py
@@ -22,24 +22,12 @@ API_HASH = environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
 # --------------M----------------------------------
 
 import os
-from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "24473318"))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8085201369:AAGAch2LfumVz0ATPXvdC1VPQmWK5KZpfoo")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("TesttxcorestBot")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "5840594311"))
-# ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5840594311").split()))
-# ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002674886829"))
-# ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://mongodburl9721:rfNOuo4c1OSTZ7cs@cluster0.edezeww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002674886829"))
-
+os.environ["API_ID"] = "34677259"
+os.environ["API_HASH"] = "f149c400ef1ee7662fcd6e74d6529d5b"
+os.environ["BOT_TOKEN"] = "8922703959:AAFtLcR2M1IsyHjvqzwVAuT2ksGM58jOZtc"
+os.environ["OWNER_ID"] = "5968883359"
+os.environ["SUDO_USERS"] = "5968883359"
+os.environ["MONGO_URL"] = "mongodb+srv://<db_username>:3DGoh7sYhfdKCwcx@extractordb.cu1uofn.mongodb.net/?appName=ExtractorDB"
+os.environ["CHANNEL_ID"] = "-1003881333048"
+os.environ["PREMIUM_LOGS"] = "-1003881333048"
