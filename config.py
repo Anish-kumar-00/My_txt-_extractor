101e0be99f2934add1db5bca6999a9778b61a0b4
Anish Kumar <ay6704197@gmail.com>
2026-09-03 20:21:12 +0530

    update config.py

--- config.py
+++ config.py
@@ -21,15 +21,14 @@ API_HASH = environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
 
 # --------------M----------------------------------
 
-from os import getenv
+import os
 
+API_ID = int(os.getenv("API_ID", "34677259"))
+API_HASH = os.getenv("API_HASH", "f149c400ef1ee7662fcd6e74d6529d5b")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8922703959:AAFtLcR2M1IsyHjvqzwVAuT2ksGM58jOZtc")
+OWNER_ID = int(os.getenv("OWNER_ID", "5968883359"))
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "5968883359").split()))
+MONGO_URL = os.getenv("MONGO_DB", "mongodb+srv://<db_username>:3DGoh7sYhfdKCwcx@extractordb.cu1uofn.mongodb.net/?appName=ExtractorDB")
 
-API_ID = int(getenv("API_ID", "34677259"))
-API_HASH = getenv("API_HASH", "f149c400ef1ee7662fcd6e74d6529d5b")
-BOT_TOKEN = getenv("BOT_TOKEN", "8922703959:AAFtLcR2M1IsyHjvqzwVAuT2ksGM58jOZtc")
-OWNER_ID = int(getenv("OWNER_ID", "5968883359"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5968883359").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://<db_username>:3DGoh7sYhfdKCwcx@extractordb.cu1uofn.mongodb.net/?appName=ExtractorDB")
-
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003881333048"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1003881333048"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1003881333048"))
+PREMIUM_LOGS = int(os.getenv("PREMIUM_LOGS", "-1003881333048"))
