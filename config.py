444bd30aeb083ade5c13d43ff61ad9d7954fc727
Anish Kumar <ay6704197@gmail.com>
2026-09-03 20:23:36 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,34 +1,11 @@
-"""
 from os import getenv
 
+API_ID = int(getenv("API_ID", "34677259"))
+API_HASH = getenv("API_HASH", "f149c400ef1ee7662fcd6e74d6529d5b")
+BOT_TOKEN = getenv("BOT_TOKEN", "8922703959:AAFtLcR2M1IsyHjvqzwVAuT2ksGM58jOZtc")
+OWNER_ID = int(getenv("OWNER_ID", "5968883359"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5968883359").split()))
+MONGO_URL = getenv("MONGO_DB", "mongodb+srv://<db_username>:3DGoh7sYhfdKCwcx@extractordb.cu1uofn.mongodb.net/?appName=ExtractorDB")
 
-API_ID = int(getenv("API_ID", "20937734"))
-API_HASH = getenv("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "5121141243"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5121141243").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002226641720"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002226641720")) "22746239"))
-API_HASH = environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-
-"""
-#
-
-
-
-
-# --------------M----------------------------------
-
-import os
-
-API_ID = int(os.getenv("API_ID", "34677259"))
-API_HASH = os.getenv("API_HASH", "f149c400ef1ee7662fcd6e74d6529d5b")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8922703959:AAFtLcR2M1IsyHjvqzwVAuT2ksGM58jOZtc")
-OWNER_ID = int(os.getenv("OWNER_ID", "5968883359"))
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "5968883359").split()))
-MONGO_URL = os.getenv("MONGO_DB", "mongodb+srv://<db_username>:3DGoh7sYhfdKCwcx@extractordb.cu1uofn.mongodb.net/?appName=ExtractorDB")
-
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1003881333048"))
-PREMIUM_LOGS = int(os.getenv("PREMIUM_LOGS", "-1003881333048"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003881333048"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1003881333048"))
